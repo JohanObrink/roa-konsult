a34ac12bc41af194f30a6fc31184d0a99562d474
Johan Öbrink <johan.obrink@24hr.se>
2012-09-01 16:41:19 +0200

    MMP

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+/*globals exports, require */
+var url = require('url');
 
-/*
- * GET home page.
- */
+var pages = [
+  { url: '/', name: 'Hem' }
+];
+
+var getMenu = function(req) {
+  var mnu = pages.slice();
+  var current = url.parse(req.url).pathname;
+  for(var i=0; i<mnu.length; i++) {
+    mnu[i].active = (mnu[i].url == current);
+  }
+  return mnu;
+};
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'RoA-konsult', menu: getMenu(req) });
 };
